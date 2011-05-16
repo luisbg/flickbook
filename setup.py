4cc8117e1aefb1dfd250037a688debf421ef9fc7
Luis de Bethencourt <luis@debethencourt.com>
2011-05-16 16:12:54 +0200

    general: update to git

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup
 setup(name="Flickbook",
       version="0.1",
       author="Luis de Bethencourt",
-      author_email=""
-      license = "GPL v2"
+      author_email="luis@debethencourt.com",
+      license = "GPL v2",
       package_dir = {'':'src'},
-      packages = "flickbook"
-      scripts = ["src/flickbook", "src/flickbook-old"]
+      packages = "flickbook",
+      scripts = ["src/flickbook", "src/flickbook-old"],
       data_files = [("src/gui.glade", "files/")]
       )
